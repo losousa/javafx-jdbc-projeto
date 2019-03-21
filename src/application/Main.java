84f7de165fdb5edd0c16a3208f75068a801798e2
Leonardo Oliveira <leeon.ardo@hotmail.com>
2019-03-21 15:00:51 -0300

    Design de View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -12,8 +14,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scroll = loader.load();
+			
+			scroll.setFitToHeight(true);
+			scroll.setFitToWidth(true);
+			Scene mainScene = new Scene(scroll);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
